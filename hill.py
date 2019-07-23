1cdbd4c59238bf98ea4e1316dd2f00d0a50a1d4c
arpan-k09 <48683269+arpan-k09@users.noreply.github.com>
2019-07-23 21:21:16 +0530

    Update hill.py

--- hill.py
+++ hill.py
@@ -1,62 +1,74 @@
-# Python3 code to implement Hill Cipher
+def encr(msg,key):
 
-keyMatrix = [[0] * 3 for i in range(3)]
+    msg1 = msg
+    rem = len(msg1) % 3
+    r = 3 - rem
 
-# Generate vector for the message
-messageVector = [[0] for i in range(3)]
+    if rem != 0:
+        while r != 0:
+            msg1 = msg1 + 'x'
+            r = r - 1
 
-# Generate vector for the cipher
-cipherMatrix = [[0] for i in range(3)]
+    le = len(msg1)
+    count = 0
+    fi_msg = []
 
+    while count != le:
+        st =''
+        c1 = 0
+        while c1 != 3:
+            st = st + msg1[count + c1]
+            c1 = c1 + 1
 
-# Following function generates the
-# key matrix for the key string
-def getKeyMatrix(key):
-    k = 0
-    for i in range(3):
-        for j in range(3):
-            keyMatrix[i][j] = ord(key[k]) % 65
-            k += 1
-
-
-# Following function encrypts the message
-def encrypt(messageVector):
-    for i in range(3):
-        for j in range(1):
-            cipherMatrix[i][j] = 0
-            for x in range(3):
-                cipherMatrix[i][j] += (keyMatrix[i][x] *
-                                       messageVector[x][j])
-            cipherMatrix[i][j] = cipherMatrix[i][j] % 26
+        fi_msg = fi_msg + [st]
+        count = count + 3
+    #print(fi_msg)
 
 
-def HillCipher(message, key):
-    # Get key matrix from the key string
-    getKeyMatrix(key)
+    #print(msg)
 
-    # Generate vector for the message
-    for i in range(3):
-        messageVector[i][0] = ord(message[i]) % 65
+    #fi = "abcdefghijklmnopqrstuvwxyz"
+    #count = 0
+    #print(rem1)
 
-    # Following function generates
-    # the encrypted vector
-    encrypt(messageVector)
+    key_l = len(key)
+    co1 = 0
+    key_li = []
+    while key_l > 0:
+        ms = ''
+        c = 0
+        while c != 3:
+            ms = ms + key[co1+c]
+            c = c + 1
 
-    # Generate the encrypted text
-    # from the encrypted vector
-    CipherText = []
-    for i in range(3):
-        CipherText.append(chr(cipherMatrix[i][0] + 65))
+        key_li = key_li + [ms]
+        co1 = co1 + 3
+        key_l = key_l - 3
 
-        # Finally print the ciphertext
-    print("Ciphertext: ", "".join(CipherText))
+    #print(key_li)
 
+    rem1 = len(fi_msg)
+    final_count = 0
+    final_str = ''
 
-message = "ACT"
+    while final_count != rem1:
+        key_count = 0
+        while key_count != 3:
+            temp = 0
+            sum1 = 0
+            while temp != 3:
+                #f = ord(key_li[key_count][temp])%97
+                #print(f)
+                sum1 = sum1 + ((ord(key_li[key_count][temp])%97)*(ord(fi_msg[final_count][temp])%97))
+                #chr((ord(key_li[key_count][temp])%65)*(ord(fi_msg[final_count][temp])%65))%26)
+                temp = temp + 1
+            final_str = final_str + chr((sum1%26)+97)
+            key_count = key_count + 1
+        final_count = final_count + 1
+    print(final_str)
 
-
-key = "GYBNQKURP"
-
-HillCipher(message, key)
-
-
+m = "gfg"
+#m = "paymoremoney"
+key = "hillmagic"
+#key = "rrfvsvcct"
+encr(m,key)
