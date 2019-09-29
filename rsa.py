3a4d9e63b9f81943f7542dc03af8b3565b7865d7
arpan-k09 <48683269+arpan-k09@users.noreply.github.com>
2019-09-29 08:21:28 +0530

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -1,12 +1,8 @@
 def gcd_com(x, y):
-    if x > y:
-        small = y
-    else:
-        small = x
-    for i in range(1, small + 1):
+
+    for i in range(1,min(x,y) + 1):
         if ((x % i == 0) and (y % i == 0)):
             gcd = i
-
     return gcd
 
 
